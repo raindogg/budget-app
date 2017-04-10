e03b45ef5305952dd517ab9fc4065ade68f61143
wabray17 <wabray17@gmail.com>
2017-04-10 13:58:09 -0500

    More front-end

--- app/assets/javascripts/display.js
+++ app/assets/javascripts/display.js
@@ -2,10 +2,13 @@ $(document).ready( function() {
     var showButtons = document.querySelectorAll('.show-form');
     var hideButton = document.querySelector('.hide-form');
     var allForms = document.querySelectorAll('.entry-form');
+    var input = document.querySelector('.input-file');
+    var detailButtons = document.querySelectorAll('.show-details');
 
     var location = window.location.pathname;
     if(location.match(/months\/\d/g) != null){
       setUpDisplay();
+      input.addEventListener('change', inputStyle);
     }
 
 
@@ -13,6 +16,10 @@ $(document).ready( function() {
     showButtons.forEach(function(button) {
       button.addEventListener('click', displayForm);
     });
+    detailButtons.forEach(function(button) {
+      button.addEventListener('click', showDetails);
+    })
+
 
     hideButton.addEventListener('click', hideForms);
   }
@@ -34,6 +41,21 @@ $(document).ready( function() {
       button.style = "display:inline-block";
     })
     hideButton.style = "display:none";
+  }
+
+  function showDetails() {
+    var catRow = this.parentElement;
+    var otherCats = document.querySelectorAll('.expense-category');
+    otherCats.forEach(function(box) {
+      box.style = "flex:1";
+      box.querySelector('.category-details').style = "display:none";
+    })
+    catRow.querySelector('.category-details').style = "display:inline-block";
+    catRow.style = "flex:5";
+  }
+
+  function inputStyle() {
+    document.querySelector('.form-label').innerHTML = this.files[0].name;
   }
 
 })
